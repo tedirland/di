82d7c95f1ef57ca5d8a19f350f86901b1ac8bba9
tedirland <irlandth@gmail.com>
2023-09-27 16:17:34 -0500

    set up di between power and cpu modules

--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
+  // These are private
   providers: [PowerService],
+  // These are public
+  exports: [PowerService],
 })
 export class PowerModule {}
